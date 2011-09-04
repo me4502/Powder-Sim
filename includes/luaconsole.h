679c7d9ce807789aad02a257a71797ba07926da3
me4502 <mnmiller1@hotmail.com>
2011-09-04 18:26:31 +1000

    fixed some stuff

--- includes/luaconsole.h
+++ includes/luaconsole.h
@@ -5,9 +5,9 @@
 #include <lauxlib.h>
 #include <lualib.h>
 #else
-#include <lua5.1/lua.h>
-#include <lua5.1/lauxlib.h>
-#include <lua5.1/lualib.h>
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
 #endif
 #include <defines.h>
 
