1a2168763cb126051f970373cf6629195e48f6ca
me4502 <mnmiller1@hotmail.com>
2011-10-10 17:22:31 +1000

    mo' fixes

--- src/elements/lcry.c
+++ src/elements/lcry.c
@@ -34,28 +34,36 @@ int graphics_LCRY(GRAPHIC_FUNC_ARGS)
     {
         if (cmode == CM_FIRE||cmode==CM_BLOB || cmode==CM_FANCY)
         {
-            if (decorations_enable && parts[i].dcolour!=NULL){
+            if (decorations_enable && parts[i].dcolour!=NULL)
+            {
                 cr = (parts[i].dcolour>>16)&0xFF;
                 cg = (parts[i].dcolour>>8)&0xFF;
                 cb = (parts[i].dcolour)&0xFF;
                 vid[ny*(XRES+BARSIZE)+nx] = parts[i].dcolour;
-            } else {
+                x = nx/CELL;
+                y = ny/CELL;
+                fire_g[y][x] = cg;
+                fire_b[y][x] = cb;
+                fire_r[y][x] = cr;
+            }
+            else
+            {
                 cr = 0x1;
                 cg = 0x1;
                 cb = 0x1;
                 vid[ny*(XRES+BARSIZE)+nx] = ptypes[t].pcolors;
+                x = nx/CELL;
+                y = ny/CELL;
+                cg += fire_g[y][x];
+                if (cg > 255) cg = 255;
+                fire_g[y][x] = cg;
+                cb += fire_b[y][x];
+                if (cb > 255) cb = 255;
+                fire_b[y][x] = cb;
+                cr += fire_r[y][x];
+                if (cr > 255) cr = 255;
+                fire_r[y][x] = cr;
             }
-            x = nx/CELL;
-            y = ny/CELL;
-            //cg += fire_g[y][x];
-            if (cg > 255) cg = 255;
-            fire_g[y][x] = cg;
-            //cb += fire_b[y][x];
-            if (cb > 255) cb = 255;
-            fire_b[y][x] = cb;
-            //cr += fire_r[y][x];
-            if (cr > 255) cr = 255;
-            fire_r[y][x] = cr;
         }
     }
     else
