a17896b19c2748af803a9567b38493e696389b51
me4502 <mnmiller1@hotmail.com>
2011-10-10 16:48:57 +1000

    fix wierd lcry glitches

--- src/elements/lcry.c
+++ src/elements/lcry.c
@@ -28,71 +28,86 @@ int update_LCRY(UPDATE_FUNC_ARGS)
 
 int graphics_LCRY(GRAPHIC_FUNC_ARGS)
 {
-	uint8 GR = 0x50+((parts[i].life>10?10:parts[i].life)*10);
-	vid[ny*(XRES+BARSIZE)+nx] = PIXRGB(GR, GR, GR);
-	if (parts[i].life>9)
-	{
-		if (cmode == CM_FIRE||cmode==CM_BLOB || cmode==CM_FANCY)
-		{
-			cr = 0x1;
-			cg = 0x1;
-			cb = 0x1;
-			vid[ny*(XRES+BARSIZE)+nx] = ptypes[t].pcolors;
-			x = nx/CELL;
-			y = ny/CELL;
-			cg += fire_g[y][x];
-			if (cg > 255) cg = 255;
-				fire_g[y][x] = cg;
-				cb += fire_b[y][x];
-			if (cb > 255) cb = 255;
-				fire_b[y][x] = cb;
-				cr += fire_r[y][x];
-			if (cr > 255) cr = 255;
-				fire_r[y][x] = cr;
-		}
-	}
-    	if (decorations_enable && cmode!=CM_HEAT && cmode!=CM_LIFE && parts[i].dcolour)
-		if(t==PT_LCRY)
-		{
-			cr = (parts[i].dcolour>>16)&0xFF;
-			cg = (parts[i].dcolour>>8)&0xFF;
-			cb = (parts[i].dcolour)&0xFF;
-					
-			if(parts[i].life<10)
-			{
-				cr /= 10-parts[i].life;
-				cg /= 10-parts[i].life;
-				cb /= 10-parts[i].life;
-			}
-			if (parts[i].life>9)
-			{
-				if (cmode == CM_FIRE||cmode==CM_BLOB || cmode==CM_FANCY)
-				{
-					cr = ((parts[i].dcolour>>16)&0xFF);
-					cg = ((parts[i].dcolour>>8)&0xFF);
-					cb = ((parts[i].dcolour)&0xFF);
-					vid[ny*(XRES+BARSIZE)+nx] = ptypes[t].pcolors;
-					x = nx/CELL;
-					y = ny/CELL;
-					cg += fire_g[y][x];
-					if (cg > 255) cg = 255;
-					fire_g[y][x] = cg;
-					cb += fire_b[y][x];
-					if (cb > 255) cb = 255;
-					fire_b[y][x] = cb;
-					cr += fire_r[y][x];
-					if (cr > 255) cr = 255;
-					fire_r[y][x] = cr;
-				}
-			}
-			/*cr = cr>255?255:cr;
-			cg = cg>255?255:cg;
-			cb = cb>255?255:cb;*/
-			blendpixel(vid, nx, ny, cr, cg, cb, (parts[i].dcolour>>24)&0xFF);
-				} 
-			else {
-					blendpixel(vid, nx, ny, (parts[i].dcolour>>16)&0xFF, (parts[i].dcolour>>8)&0xFF, (parts[i].dcolour)&0xFF, (parts[i].dcolour>>24)&0xFF);
-				}
+    uint8 GR = 0x50+((parts[i].life>10?10:parts[i].life)*10);
+    vid[ny*(XRES+BARSIZE)+nx] = PIXRGB(GR, GR, GR);
+    if (parts[i].life>9)
+    {
+        if (cmode == CM_FIRE||cmode==CM_BLOB || cmode==CM_FANCY)
+        {
+            if (decorations_enable && parts[i].dcolour!=NULL){
+                cr = (parts[i].dcolour>>16)&0xFF;
+                cg = (parts[i].dcolour>>8)&0xFF;
+                cb = (parts[i].dcolour)&0xFF;
+                vid[ny*(XRES+BARSIZE)+nx] = parts[i].dcolour;
+            } else {
+                cr = 0x1;
+                cg = 0x1;
+                cb = 0x1;
+                vid[ny*(XRES+BARSIZE)+nx] = ptypes[t].pcolors;
+            }
+            x = nx/CELL;
+            y = ny/CELL;
+            cg += fire_g[y][x];
+            if (cg > 255) cg = 255;
+            fire_g[y][x] = cg;
+            cb += fire_b[y][x];
+            if (cb > 255) cb = 255;
+            fire_b[y][x] = cb;
+            cr += fire_r[y][x];
+            if (cr > 255) cr = 255;
+            fire_r[y][x] = cr;
+        }
+    }
+    else
+    {
+        x = nx/CELL;
+        y = ny/CELL;
+        fire_g[y][x] = 0;
+        fire_b[y][x] = 0;
+        fire_r[y][x] = 0;
+        uint8 GR = 0x50+((parts[i].life>10?10:parts[i].life)*10);
+        vid[ny*(XRES+BARSIZE)+nx] = PIXRGB(GR, GR, GR);
+    }
+    /*if (decorations_enable && cmode!=CM_HEAT && cmode!=CM_LIFE && parts[i].dcolour){
+            cr = (parts[i].dcolour>>16)&0xFF;
+            cg = (parts[i].dcolour>>8)&0xFF;
+            cb = (parts[i].dcolour)&0xFF;
 
-	return 0;
+            if(parts[i].life<10)
+            {
+                cr /= 10-parts[i].life;
+                cg /= 10-parts[i].life;
+                cb /= 10-parts[i].life;
+            }
+            if (parts[i].life>9)
+            {
+                if (cmode == CM_FIRE||cmode==CM_BLOB || cmode==CM_FANCY)
+                {
+                    cr = ((parts[i].dcolour>>16)&0xFF);
+                    cg = ((parts[i].dcolour>>8)&0xFF);
+                    cb = ((parts[i].dcolour)&0xFF);
+                    vid[ny*(XRES+BARSIZE)+nx] = ptypes[t].pcolors;
+                    x = nx/CELL;
+                    y = ny/CELL;
+                    cg += fire_g[y][x];
+                    if (cg > 255) cg = 255;
+                    fire_g[y][x] = cg;
+                    cb += fire_b[y][x];
+                    if (cb > 255) cb = 255;
+                    fire_b[y][x] = cb;
+                    cr += fire_r[y][x];
+                    if (cr > 255) cr = 255;
+                    fire_r[y][x] = cr;
+                }
+            }
+            /*cr = cr>255?255:cr;
+            cg = cg>255?255:cg;
+            cb = cb>255?255:cb;*/
+            //blendpixel(vid, nx, ny, cr, cg, cb, (parts[i].dcolour>>24)&0xFF);
+    //}else{
+    //    blendpixel(vid, nx, ny, cr, cg, cb, (ptypes[t].pcolors>>24)&0xFF);
+    //}
+
+
+    return 0;
 }
