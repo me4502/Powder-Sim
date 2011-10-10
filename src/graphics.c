193e5a25c186076966158f36404fe7a3c49db131
me4502 <mnmiller1@hotmail.com>
2011-10-10 16:48:57 +1000

    fix wierd lcry glitches

--- src/graphics.c
+++ src/graphics.c
@@ -2983,13 +2983,13 @@ void draw_parts(pixel *vid)
                 {
                     if (!(*(ptypes[t].graphic_func))(i,nx,ny,vid,cr,cg,cb,t,x,y,fr,fg,fb))
                     {
-                        if (parts[i].dcolour != NULL)
+                        if (parts[i].dcolour != NULL && decorations_enable)
                         {
                             vid[ny*(XRES+BARSIZE)+nx] = parts[i].dcolour;
                         }
                         continue;
                     }
-                    else if (parts[i].dcolour != NULL)
+                    else if (parts[i].dcolour != NULL && decorations_enable)
                     {
                         vid[ny*(XRES+BARSIZE)+nx] = parts[i].dcolour;
                     }
@@ -3108,46 +3108,6 @@ void draw_parts(pixel *vid)
 				blendpixel(vid, nx+1, ny+1, cr, cg, cb, 112);
 				blendpixel(vid, nx-1, ny+1, cr, cg, cb, 112);
 			}
-			if (decorations_enable && cmode!=CM_HEAT && cmode!=CM_LIFE && parts[i].dcolour)
-				if(t==PT_LCRY)
-				{
-					cr = (parts[i].dcolour>>16)&0xFF;
-					cg = (parts[i].dcolour>>8)&0xFF;
-					cb = (parts[i].dcolour)&0xFF;
-
-					if(parts[i].life<10){
-						cr /= 10-parts[i].life;
-						cg /= 10-parts[i].life;
-						cb /= 10-parts[i].life;
-					}
-					if (parts[i].life>9)
-					{
-						if (cmode == CM_FIRE||cmode==CM_BLOB || cmode==CM_FANCY)
-						{
-							cr = ((parts[i].dcolour>>16)&0xFF);
-							cg = ((parts[i].dcolour>>8)&0xFF);
-							cb = ((parts[i].dcolour)&0xFF);
-							vid[ny*(XRES+BARSIZE)+nx] = ptypes[t].pcolors;
-							x = nx/CELL;
-							y = ny/CELL;
-							cg += fire_g[y][x];
-							if (cg > 255) cg = 255;
-							fire_g[y][x] = cg;
-							cb += fire_b[y][x];
-							if (cb > 255) cb = 255;
-							fire_b[y][x] = cb;
-							cr += fire_r[y][x];
-							if (cr > 255) cr = 255;
-							fire_r[y][x] = cr;
-						}
-					}
-					/*cr = cr>255?255:cr;
-					cg = cg>255?255:cg;
-					cb = cb>255?255:cb;*/
-					blendpixel(vid, nx, ny, cr, cg, cb, (parts[i].dcolour>>24)&0xFF);
-				} else {
-					blendpixel(vid, nx, ny, (parts[i].dcolour>>16)&0xFF, (parts[i].dcolour>>8)&0xFF, (parts[i].dcolour)&0xFF, (parts[i].dcolour>>24)&0xFF);
-				}
 		}
 #endif
 	}
