235ed851a675b747f10bd373cbdf2c08a32130b1
me4502 <mnmiller1@hotmail.com>
2011-10-21 17:46:21 +1000

    gave the noble gases some character

---
+++ src/elements/nble.c
@@ -0,0 +1,31 @@
+#include <element.h>
+
+int update_NBLE(UPDATE_FUNC_ARGS) {
+    int ct = parts[i].ctype;
+    if (ct==NBL_HELM)
+    {
+        if (rand()%2==1)
+            parts[i].vy -= 2;
+    }
+    else if (ct==NBL_NEON||ct==NBL_ARGN||ct==NBL_KPTN||ct==NBL_RDON||ct==NBL_XNON||ct==NBL_RDON||ct==NBL_UNCM)
+    {
+        int p = rand()%4;
+        if (p==0)
+        {
+            parts[i].vy += rand()%ct+1;
+        }
+        else if (p==1)
+        {
+            parts[i].vy -= rand()%ct+1;
+        }
+        else if (p==2)
+        {
+            parts[i].vx += rand()%ct+1;
+        }
+        else if (p==3)
+        {
+            parts[i].vx -= rand()%ct+1;
+        }
+    }
+	return 0;
+}
