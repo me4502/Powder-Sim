7c7c3c312a45008e3a4ad6b1c17c30970816b2f8
Anton <antonvrg@gmail.com>
2011-10-25 22:42:19 +1100

    FRAN updates

--- src/elements/fran.c
+++ src/elements/fran.c
@@ -9,11 +9,11 @@ int update_FRAN(UPDATE_FUNC_ARGS) {
 				r = pmap[y+ry][x+rx];
 				if (!r)
 					continue;
-				if (((r&0xFF)!=PT_FRAN && parts[r>>8].temp<100)||(r&0xFF)==PT_FIRE)
+				if (((r&0xFF)!=PT_FRAN && parts[r>>8].temp<100)||(r&0xFF)==PT_HFLM)
 				{
 					if (1>rand()%6)
 					{
-						part_change_type(i,x,y,PT_FIRE);
+						part_change_type(i,x,y,PT_HFLM);
 						parts[r>>8].temp = parts[i].temp = 0;
 						parts[i].life = rand()%150+50;
 						pv[y/CELL][x/CELL] += 1.5;
