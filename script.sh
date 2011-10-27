b6869ad2b6c6308dac66b1443da7c3f210b948da
Anton <antonvrg@gmail.com>
2011-10-27 16:25:12 +1100

    cleanup

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git pull
+#git pull
 make powder-64-sse3
 make powder-64-sse2
 make powder-sse3
@@ -7,5 +7,4 @@ make powder-debug
 make powder-sse2
 make powder
 make powder-sse
-tar czf PowderToyGit.tar.gz ./
-
+#tar czf PowderToyGit.tar.gz ./
