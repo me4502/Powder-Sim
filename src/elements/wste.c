28ef02e9c2083927d267d31d8d33b72f3aba4e36
sniper349 <vanquish349@hotmail.com>
2011-11-01 21:15:06 +1100

    fixed up waste

---
+++ src/elements/wste.c
@@ -0,0 +1,51 @@
+#include <element.h>
+
+int update_WSTE(UPDATE_FUNC_ARGS) {
+    int r, rx, ry, trade, np;
+   for (rx=-2; rx<3; rx++)
+        for (ry=-2; ry<3; ry++)
+            if (x+rx>=0 && y+ry>0 && x+rx<XRES && y+ry<YRES && (rx || ry)) {
+                r = pmap[y+ry][x+rx];
+                if (!r)
+                    continue;
+                if ((r&0xFF)!=PT_WSTE && (r&0xFF)!=PT_DMND && (r&0xFF)!=PT_URAN && (r&0xFF)!=PT_PLUT && (r&0xFF)!=PT_DEUT && (r&0xFF)!=PT_NEUT && (r&0xFF)!=PT_WATR) {
+                    if ((r&0xFF)==PT_PLEX || (r&0xFF)==PT_NITR || (r&0xFF)==PT_GUNP || (r&0xFF)==PT_RBDM || (r&0xFF)==PT_LRBD) {
+                        part_change_type(i,x,y,PT_FIRE);
+                        part_change_type(r>>8,x+rx,y+ry,PT_FIRE);
+                        parts[r>>8].life = 4;
+                    }
+                    else if (parts[i].life < 2) 
+			part_change_type(i,x,y,PT_GLOW);
+		    else 
+	            {
+			if (rand()%700 == 1) {                        
+				kill_part(r>>8);
+                        	parts[i].life -= parts[i].life -1;
+				return 1;
+               			} 
+                        }
+                    }
+                }
+   /* for ( trade = 0; trade<2; trade ++) {
+        rx = rand()%5-2;
+        ry = rand()%5-2;
+        if (x+rx>=0 && y+ry>0 && x+rx<XRES && y+ry<YRES && (rx || ry)) {
+            r = pmap[y+ry][x+rx];
+            if ((r>>8)>=NPART || !r)
+                continue;
+            if ((r&0xFF)==PT_WSTE&&(parts[i].life>parts[r>>8].life)&&parts[i].life>0) {//diffusion
+                int temp = parts[i].life - parts[r>>8].life;
+                if (temp ==1) {
+                    parts[r>>8].life ++;
+                    parts[i].life --;
+                }
+                else if (temp>0) {
+                    parts[r>>8].life += temp/2;
+                    parts[i].life -= temp/2;
+                }
+            }
+        }
+    }*/
+    return 0;
+}
+
