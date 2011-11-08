d0d2be86c2c41e780ce170195360b809c81b4744
me4502 <mnmiller1@hotmail.com>
2011-11-08 19:11:28 +1000

    fixed alco fire

---
+++ src/elements/alco.c
@@ -0,0 +1,18 @@
+#include <element.h>
+
+int update_ALCO(UPDATE_FUNC_ARGS)
+{
+    int r, rx, ry, rt;
+    for (rx=-1; rx<2; rx++)
+        for (ry=-1; ry<2; ry++)
+            if (x+rx>=0 && y+ry>0 && x+rx<XRES && y+ry<YRES && (rx || ry))
+            {
+                r = pmap[y+ry][x+rx];
+                if (!r)
+                    continue;
+                rt = parts[r>>8].type;
+                if (parts[i].temp >= 315.15f+273.15f && rt!=PT_FIRE && rt!=PT_LAVA && rt!=PT_PLSM)
+                    part_change_type(i,x,y,PT_GAS);
+            }
+    return 0;
+}
