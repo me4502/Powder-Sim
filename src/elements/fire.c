dfa253a1fa2e956154bc4c0387d1c6e78cc9ab01
sniper349 <vanquish349@hotmail.com>
2011-11-08 20:23:56 +1100

    added fire colours for differet elements, current elements: alco, coal

--- src/elements/fire.c
+++ src/elements/fire.c
@@ -2,11 +2,25 @@
 
 int graphics_FIRE(GRAPHICS_FUNC_ARGS)
 {
-	int caddress = restrict_flt(restrict_flt((float)cpart->life, 0.0f, 200.0f)*3, 0.0f, (200.0f*3)-3);
-	*colr = (unsigned char)flm_data[caddress];
-	*colg = (unsigned char)flm_data[caddress+1];
-	*colb = (unsigned char)flm_data[caddress+2];
-	
+	int caddress = restrict_flt(restrict_flt((float)cpart->life, 0.0f, 200.0f)*3, 0.0f, (200.0f*3)-3);	
+	if (cpart-> tmp == PT_ALCO)
+	{
+		*colr = (unsigned char)flm_data[caddress];
+		*colg = (unsigned char)flm_data[caddress];
+		*colb = (unsigned char)flm_data[caddress];
+	} 	
+	else if (cpart-> tmp == PT_COAL)
+	{		
+		*colr = (unsigned char)flm_data[caddress+1];
+		*colg = (unsigned char)flm_data[caddress+2];
+		*colb = (unsigned char)flm_data[caddress+2];
+	}
+	else
+	{		
+		*colr = (unsigned char)flm_data[caddress];
+		*colg = (unsigned char)flm_data[caddress+1];
+		*colb = (unsigned char)flm_data[caddress+2];
+	}		
 	*firea = 255;
 	*firer = *colr;
 	*fireg = *colg;
