07aae676046ba19abcb0ac0bad316250a8f6352e
me4502 <mnmiller1@hotmail.com>
2011-11-11 19:44:25 +1000

    vast improvements

--- src/elements/alco.c
+++ src/elements/alco.c
@@ -3,7 +3,7 @@
 int update_ALCO(UPDATE_FUNC_ARGS)
 {
     int r, rx, ry, rt;
-    
+
     if(!(rand()%200))
     {
         parts[i].tmp2 = rand()%40;
@@ -12,7 +12,7 @@ int update_ALCO(UPDATE_FUNC_ARGS)
     {
         parts[i].tmp2 -= (parts[i].tmp2>20)?1:-1;
     }
-    
+
     for (rx=-1; rx<2; rx++)
         for (ry=-1; ry<2; ry++)
             if (x+rx>=0 && y+ry>0 && x+rx<XRES && y+ry<YRES && (rx || ry))
@@ -23,6 +23,7 @@ int update_ALCO(UPDATE_FUNC_ARGS)
                 rt = parts[r>>8].type;
                 if (parts[i].temp >= 315.15f+273.15f && rt!=PT_FIRE && rt!=PT_LAVA && rt!=PT_PLSM)
                     part_change_type(i,x,y,PT_GAS);
+                    parts[i].ctype = PT_ALCO;
             }
     return 0;
 }
