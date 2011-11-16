9aec7e9b01074eeb947571b3ff57558d75e105b2
me4502 <mnmiller1@hotmail.com>
2011-11-16 16:30:14 +1000

    fixy fixy dwarf

--- src/elements/neut.c
+++ src/elements/neut.c
@@ -129,14 +129,16 @@ int update_NEUT(UPDATE_FUNC_ARGS)
                 else if ((r&0xFF)==PT_ACID && 5>(rand()%100))
                     create_part(r>>8, x+rx, y+ry, PT_ISOZ);
                 else if ((r&0xFF)==PT_BIZR && 5>(rand()%100))
-                ge = rand()%PT_NUM;
-                if (ge == PT_LOVE||ge == PT_LOLZ||ge == PT_VIRS)
                 {
-                    create_part(r>>8, x+rx, y+ry, PT_DUST);
-                }
-                else
-                {
-                    create_part(r>>8, x+rx, y+ry, ge);
+                    ge = rand()%PT_NUM;
+                    if (ge == PT_LOVE||ge == PT_LOLZ||ge == PT_VIRS||ge == PT_STKM||ge == PT_STKM2)
+                    {
+                        create_part(r>>8, x+rx, y+ry, PT_DUST);
+                    }
+                    else
+                    {
+                        create_part(r>>8, x+rx, y+ry, ge);
+                    }
                 }
 
 		  /*if(parts[r>>8].type>1 && parts[r>>8].type!=PT_NEUT && parts[r>>8].type-1!=PT_NEUT && parts[r>>8].type-1!=PT_STKM &&
