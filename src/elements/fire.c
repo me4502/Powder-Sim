486aeeda776947427a14fc96dabd6c94c04a7982
me4502 <mnmiller1@hotmail.com>
2011-11-17 16:56:27 +1000

    better code

--- src/elements/fire.c
+++ src/elements/fire.c
@@ -6,45 +6,56 @@ int graphics_FIRE(GRAPHICS_FUNC_ARGS)
 	int fr, fg, fb;
 	int *pix;
 	int *pixf;
+	int deftype, cleartype;
 
 	if (cpart-> tmp == PT_ALCO)
 	{
 		fr = 0;
 		fg = 0;
 		fb = 0;
+		deftype = 1;
+		cleartype = 1;
 	}
 	else if (cpart-> tmp == PT_COAL || cpart-> tmp == PT_BCOL)
 	{
 		fr = 1;
 		fg = 2;
 		fb = 2;
+		deftype = 1;
+		cleartype = 1;
 	}
 	else if (cpart-> tmp == PT_NITR)
 	{
 		fr = 2;
 		fg = 1;
 		fb = 2;
+		deftype = 1;
+		cleartype = 1;
 	}
 	else if (cpart-> tmp == PT_FRAN)
 	{
 		fr = 2;
 		fg = 2;
 		fb = 1;
+		deftype = 1;
+		cleartype = 1;
 	}
 	else if (cpart-> tmp == PT_MAGN)
 	{
 		fr = 1;
 		fg = 1;
 		fb = 1;
-
-
-
+		*pixel_mode |= PMODE_SPARK;
+		deftype = 0;
+		cleartype = 1;
 	}
 	else
 	{
 		fr = 0;
 		fg = 1;
 		fb = 2;
+		deftype = 1;
+		cleartype = 1;
 	}
 
 	*colr = (unsigned char)flm_data[caddress+fr];
@@ -56,11 +67,9 @@ int graphics_FIRE(GRAPHICS_FUNC_ARGS)
 	*fireg = *colg;
 	*fireb = *colb;
 
-	*pixel_mode = PMODE_NONE; //Clear default, don't draw pixel
-	if (cpart-> tmp == PT_MAGN)
-		*pixel_mode |= PMODE_SPARK;	
-		//*pixel_mode |= PMODE_FLARE;
-	else
+    if (cleartype)
+        *pixel_mode = PMODE_NONE; //Clear default, don't draw pixel
+	if (deftype)
 		*pixel_mode |= FIRE_ADD;
 	//Returning 0 means dynamic, do not cache
 	return 0;
