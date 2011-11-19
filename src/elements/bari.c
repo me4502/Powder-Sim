e6a9abfc9eb17de5e2e7a75f7b610cf487a0c28a
sniper349 <vanquish349@hotmail.com>
2011-11-19 20:20:15 +1100

    fixed up barium a bit, still deosnt work as intented though

--- src/elements/bari.c
+++ src/elements/bari.c
@@ -1,16 +1,18 @@
 #include <element.h>
 
-int update_BARI(UPDATE_FUNC_ARGS)
-{
-	int r, rx, ry, rt;
-	
+int update_BARI(UPDATE_FUNC_ARGS) {
+	int r, rx, ry;
 	for (rx=-2; rx<3; rx++)
 		for (ry=-2; ry<3; ry++)
 			if (x+rx>=0 && y+ry>0 && x+rx<XRES && y+ry<YRES && (rx || ry))
 			{
-				if ((r&0xFF)==PT_ALCO || (r&0xFF)==PT_WATR) {
-					part_change_type(i,x,y,PT_FIRE);
-					part_change_type(r>>8,x+rx,y+ry,PT_FIRE);
+				r = pmap[y+ry][x+rx];
+               			if (!r)
+					continue;
+				if ((r&0xFF)==PT_ALCO  || (r&0xFF)==PT_WATR) 
+				{					
+                      			part_change_type(i,x,y,PT_FIRE);
+                        		part_change_type(r>>8,x+rx,y+ry,PT_FIRE);
 				}
 			}
 	return 0;
