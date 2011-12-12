e13e151945b7fac26555f2a74986a1e367d21c50
me4502 <mnmiller1@hotmail.com>
2011-12-12 16:42:35 +1000

    neut+virs = wste watr+virs = rwtr

--- src/elements/neut.c
+++ src/elements/neut.c
@@ -128,6 +128,10 @@ int update_NEUT(UPDATE_FUNC_ARGS)
                     parts[r>>8].ctype = PT_DUST;
                 else if ((r&0xFF)==PT_ACID && 5>(rand()%100))
                     create_part(r>>8, x+rx, y+ry, PT_ISOZ);
+                else if ((r&0xFF)==PT_VIRS && 5>(rand()%100))
+                    create_part(r>>8, x+rx, y+ry, PT_WSTE);
+                else if ((r&0xFF)==PT_WATR && 5>(rand()%100))
+                    parts[r>>8].tmp2 += rand()%50;
                 else if (((r&0xFF)==PT_BIZR||(r&0xFF)==PT_BIZRG||(r&0xFF)==PT_BIZRS) && 5>(rand()%100))
                 {
                     ge = rand()%PT_NUM;
