5ca3f55ee1d23d09cb8e5579940e907b2c9b5877
sniper349 <vanquish349@hotmail.com>
2011-12-18 18:12:04 +1100

    fixed up intro message

--- src/main.c
+++ src/main.c
@@ -133,7 +133,7 @@ static const char *it_msg =
     "\r\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\n"
     "\n"
     "\bgControl+C/V/X are Copy, Paste and cut respectively.\n"
-    "\bgTo choose a material, hover over one of the icons on the right, it will show a selection of elements in that group.\n"
+    "\bgTo choose a material, hover over one of the icons on the right,\n it will show a selection of elements in that group.\n"
     "\bgPick your material from the menu using mouse left/right buttons.\n"
     "Draw freeform lines by dragging your mouse left/right button across the drawing area.\n"
     "Shift+drag will create straight lines of particles.\n"
@@ -141,22 +141,18 @@ static const char *it_msg =
     "Ctrl+Shift+click will flood-fill a closed area.\n"
     "Ctrl+Z will act as Undo.\n"
     "Middle click or Alt+Click to \"sample\" the particles.\n"
-    "\n\boUse 'Z' for a zoom tool. Click to make the drawable zoom window stay around. Use the wheel to change the zoom strength\n"
+    "\n\boUse 'Z' for a zoom tool. Click to make the drawable zoom window stay around.\n Use the wheel to change the zoom strength\n"
     "Use 'S' to save parts of the window as 'stamps'.\n"
     "'L' will load the most recent stamp, 'K' shows a library of stamps you saved.\n"
-    "'C' will cycle the display mode (Fire, Blob, Velocity, etc.). The numbers on the keyboard do the same\n"
-    "Use the mouse scroll wheel to change the tool size for particles.\n"
-    "The spacebar can be used to pause physics.\n"
-    "'P' will take a screenshot and save it into the current directory.\n"
     "\n"
-    "\boPowder Sim Contributors:\bg Madeline Miller(me4502), Vanquish349, Randalserrano And Antonvrg\n\n"
+    "\boPowder Sim Contributors:\bg Madeline Miller(me4502), Vanquish349, Randalserrano And Antonvrg\n"
+    "Font 'installer' orignaly by Milan Zrnic\n\n"
 
-
-    "\boPowder Toy Contributors: \bgStanislaw K Skowronek (\brhttp://powder.unaligned.org\bg, \bbirc.unaligned.org #wtf\bg),\n"
-    "\bgSimon Robertshaw, Skresanov Savely, cracker64, Catelite, Bryan Hoyle, Nathan Cousins, jacksonmj,\n"
-	"\bgLieuwe Mosch, Anthony Boot, Matthew \"me4502\", MaksProg\n"
+    "\boPowder Toy Contributors: \bgStanislaw K Skowronek (\brhttp://powder.unaligned.org\bg,\n \bbirc.unaligned.org #wtf\bg),"
+    "\bgSimon Robertshaw, Skresanov Savely, cracker64, Catelite,\n Bryan Hoyle, Nathan Cousins, jacksonmj,"
+	"\bgLieuwe Mosch, Anthony Boot,\n Matthew \"me4502\", MaksProg\n"
     "\n"
-    "\bgTo use online features such as saving, you need to register at: \brhttp://powdertoy.co.uk/Register.html"
+    "\bgTo use online features such as saving, you need to register at:\n \brhttp://powdertoy.co.uk/Register.html"
     ;
 
 typedef struct
