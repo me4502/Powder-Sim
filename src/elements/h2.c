68539f0d70102998fa880e3c508326111b18c1d9
me4502 <mnmiller1@hotmail.com>
2012-01-05 21:49:42 +1000

    fixed deuterium/tritium glitch

--- src/elements/h2.c
+++ src/elements/h2.c
@@ -49,7 +49,7 @@ int update_H2(UPDATE_FUNC_ARGS)
                 else if (parts[i].ctype==3)
                 {
                     if ((r&0xFF)==PT_NEUT && parts[r>>8].tmp3==PT_DEUT)
-                        parts[i].ctype=3;
+                        parts[i].ctype=4;
                     if ((r&0xFF)==PT_WATR||(r&0xFF)==PT_DSTW)
                         parts[r>>8].tmp2++;
                 }
